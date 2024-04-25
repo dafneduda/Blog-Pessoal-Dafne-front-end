b79428c0201c189044496924813f6a882412ddbf
Dafne Duda <dafne.leslie@gmail.com>
2024-04-25 12:37:56 -0300

    estilos

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,5 +1,5 @@
 import { useContext } from 'react';
-import homeLogo from '../../assets/img/homeLogo.jpg'
+import login2 from '../../assets/img/login2.jpg'
 import ListaPostagens from '../../components/postagens/listaPostagens/ListaPostagens';
 import './Home.css';
 import ModalPostagem from '../../components/postagens/modalPostagem/ModalPostagem';
@@ -11,7 +11,7 @@ const {usuario} = useContext(AuthContext)
 
   return (
     <>
-      <div className="bg-pink-900 flex justify-center">
+      <div className="bg-pink-300 flex justify-center">
         <div className='container grid grid-cols-2 text-white'>
           <div className="flex flex-col gap-4 items-center justify-center py-4">
             <h2 className='text-5xl font-bold'>Olá, {usuario.nome}!</h2>
@@ -25,7 +25,7 @@ const {usuario} = useContext(AuthContext)
           </div>
 
           <div className="flex justify-center ">
-            <img src={homeLogo} alt="" className='w-2/3 rounded-full' />
+            <img src={login2} alt="" className='w-2/3 rounded-full' />
 
           </div>
         </div>
