15f62b3ded14eca5525426138675840e0dc50cd1
Dafne Duda <dafne.leslie@gmail.com>
2024-05-17 13:00:03 -0300

    responsividade Home

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,32 +1,33 @@
 import { useContext } from 'react';
-import login2 from '../../assets/img/login2.jpg'
+import login2 from '../../assets/img/login2.jpg';
 import ListaPostagens from '../../components/postagens/listaPostagens/ListaPostagens';
-import './Home.css';
 import ModalPostagem from '../../components/postagens/modalPostagem/ModalPostagem';
 import { Link } from 'react-router-dom';
 import { AuthContext } from '../../contexts/AuthContext';
+import './Home.css';
 
 function Home() {
-const {usuario} = useContext(AuthContext)
+  const { usuario } = useContext(AuthContext);
 
   return (
     <>
       <div className="bg-pink-300 flex justify-center">
-        <div className='container grid grid-cols-2 text-white'>
+        <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 text-white p-4">
           <div className="flex flex-col gap-4 items-center justify-center py-4">
-            <h2 className='text-5xl font-bold'>Olá, {usuario.nome}!</h2>
-            <p className='text-xl'>Expresse aqui seus pensamentos e opiniões :)</p>
-
-            <div className="flex justify-around gap-4">
-            <ModalPostagem />
-
-             <Link to="/postagens" > <button className='rounded bg-pink text-white-800 py-2 px-4 border-white'>Ver postagens</button></Link>
+            <h2 className="text-3xl md:text-5xl font-bold text-center">Olá, {usuario.nome}!</h2>
+            <p className="text-lg md:text-xl text-center">Expresse aqui seus pensamentos e opiniões :)</p>
+            <div className="flex flex-col md:flex-row justify-around gap-4">
+              <ModalPostagem />
+              <Link to="/postagens">
+                <button className="rounded bg-pink-500 text-white py-2 px-4 border-white">
+                  Ver postagens
+                </button>
+              </Link>
             </div>
           </div>
 
-          <div className="flex justify-center ">
-            <img src={login2} alt="" className='w-2/3 rounded-full' />
-
+          <div className="flex justify-center py-4 md:py-0">
+            <img src={login2} alt="login" className="w-2/3 md:w-full rounded-full" />
           </div>
         </div>
       </div>
