fb6151a76e53d1dadeaf69625f6cb5a195c05e3d
Dafne Duda <dafne.leslie@gmail.com>
2024-05-17 13:05:52 -0300

    responsividade navBar

--- src/components/navBar/NavBar.tsx
+++ src/components/navBar/NavBar.tsx
@@ -5,7 +5,6 @@ import { toastAlerta } from "../../utils/toastAlerta";
 
 function Navbar() {
   let navigate = useNavigate();
-
   const { usuario, handleLogout } = useContext(AuthContext);
 
   function logout() {
@@ -14,22 +13,20 @@ function Navbar() {
     navigate("/login");
   }
 
-  let navbarComponent;
-
   return (
     <>
       <div className="w-full bg-pink-900 text-white flex justify-center py-4">
-        <div className="container flex justify-between text-lg">
-          <Link to="/home" className="text-2xl font-bold uppercase">
+        <div className="container mx-auto flex flex-col md:flex-row items-center justify-between text-lg">
+          <Link to="/home" className="text-2xl font-bold uppercase mb-4 md:mb-0">
             Blog Pessoal
           </Link>
 
-          <div className="flex gap-4">
+          <div className="flex flex-col md:flex-row items-center gap-4">
             <Link to="/postagens" className="hover:underline">Postagens</Link>
             <Link to="/temas" className="hover:underline">Temas</Link>
             <Link to="/cadastroTema" className="hover:underline">Cadastrar tema</Link>
-            <Link to='/perfil' className='hover:underline'>Perfil</Link>
-            <img src={usuario.foto} className="w-9 h-full rounded-full border-purple-400 border-4"/>
+            <Link to="/perfil" className="hover:underline">Perfil</Link>
+            <img src={usuario.foto} alt="Perfil" className="w-9 h-9 rounded-full border-purple-400 border-4" />
             <Link to="" onClick={logout} className="hover:underline">Sair</Link>
           </div>
         </div>
